9001a43764ef774245feff6e11f90b29ea1ad79b
MoonSsunny <moonsun116@naver.com>
2024-11-11 01:51:55 +0900

    [feat] 기본셋팅

---
+++ eslint.config.js
@@ -0,0 +1,84 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        webextensions: true,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: {
+      react: { version: '18.3' },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx'],
+        },
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+      prettier,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+        },
+      ],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'no-var': 'error',
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': [
+        'warn',
+        {
+          additionalHooks: 'useRecoilCallback',
+        },
+      ],
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'import/errors': 'error',
+      'import/warnings': 'warn',
+    },
+  },
+];
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styles from './App.module.scss';
+// import Example from './components/user/Example';
+// import { date } from './utils/date';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className="App-header">
+        {/* <Example /> */}
+        {/* {date()} */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
