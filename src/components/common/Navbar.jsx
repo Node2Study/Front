16b8f93c89a544602b5d8300c7e368696f03f241
MoonSsunny <moonsun116@naver.com>
2024-11-13 00:41:25 +0900

    [feat] nav 설정

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,30 +1,36 @@
+import { useState } from 'react';
 import styles from './Navbar.module.scss';
 import { Link } from 'react-router-dom';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const menu = [
-    {
-      name: '메뉴1',
-      link: '/menu1',
-    },
-    {
-      name: '메뉴2',
-      link: '/menu2',
-    },
-    {
-      name: '메뉴3',
-      link: '/menu3',
-    },
+    { name: '메뉴1', link: '/menu1' },
+    { name: '메뉴2', link: '/menu2' },
+    { name: '메뉴3', link: '/menu3' },
   ];
+
   return (
     <nav className={styles.nav}>
       <div className={styles.container}>
         <h1>Project name</h1>
-        <div className={styles.menu}>
-          <ul className={styles.menu}>
+        <button
+          className={styles.hamburger}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          aria-label="Toggle menu"
+        >
+          <RxHamburgerMenu />
+        </button>
+
+        <div className={`${styles.menu} ${isMenuOpen ? styles.menuOpen : ''}`}>
+          <ul className={styles.menuList}>
             {menu.map((item, index) => (
               <li key={index}>
-                <Link to={item.link}>{item.name}</Link>
+                <Link to={item.link} onClick={() => setIsMenuOpen(false)}>
+                  {item.name}
+                </Link>
               </li>
             ))}
           </ul>
