29c3120ded3cca27f9b322e9a971b899294d349e
MoonSsunny <moonsun116@naver.com>
2024-11-12 23:37:03 +0900

    [feat] App router setting

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import Page from '@/pages/Page';
+
 const AppRouter = () => {
-  return <div>라우터설정필요</div>;
+  return (
+    <Routes>
+      {/* 임시입니다 */}
+      <Route path="/" element={<Page />} />
+    </Routes>
+  );
 };
 
 export default AppRouter;
