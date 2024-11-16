ad5f0e8656a8630c1fcd310c1d11a05bcd2824f3
MoonSsunny <moonsun116@naver.com>
2024-11-16 23:47:09 +0900

    [feat] 포트폴리오 아이템 컴포넌트 생성

---
+++ src/components/myPortfolio/PortfolioItem.jsx
@@ -0,0 +1,38 @@
+import styles from './PortfolioItem.module.scss';
+
+const PortfolioItem = () => {
+  return (
+    <section className={styles.item}>
+      <div className={styles.itemImage}>이미지영역</div>
+      <div className={styles.itemDetail}>
+        <div className={styles.itemExplanation}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <button type="button" className={styles.more}>
+          상세보기
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default PortfolioItem;
