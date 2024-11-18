800bc335188609ce94748c4d69fe42051dbc75e5
ChoSangok <chod1510@naver.com>
2024-11-19 02:47:55 +0900

    [feat] 액세스토큰만료시리프레시토큰전달

--- src/api/api.js
+++ src/api/api.js
@@ -1,21 +1,24 @@
 import axios from 'axios';
+import useUserStore from '../stores/useUserStore';
+import { isTokenExpired } from '../utils/isTokenExpried';
 
 const BACK_URL = import.meta.env.VITE_API_BASE_URL;
-const TOKEN = sessionStorage.getItem('token'); //임시설정, session에 할건지, cookies에 할건지 결정 필요
 
 const api = axios.create({
   baseURL: BACK_URL,
   headers: {
     'Content-Type': 'application/json',
-    authorization: `Bearer ${TOKEN}`,
   },
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token = sessionStorage.getItem('token'); //임시설정, session에 할건지, cookies에 할건지 결정 필요
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    const { accessToken } = useUserStore.getState();
+
+    if (accessToken && isTokenExpired(accessToken)) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    } else {
+      config.withCredentials = true;
     }
     return config;
   },
