4e1236f2e1fc426a3ee936c53e4932076d36f028
KILL-BURGER <dzaws@naver.com>
2024-11-20 22:27:02 +0900

    [feat] 코드 작성 스타일 수정

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,19 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import Page from '@/pages/Page';
 import Login from '@/pages/LoginPage/Login';
 import Register from '../pages/RegisterPage/Register';
-import Project from '@/pages/ProjectPage/Project';
+import Project from '../pages/ProjectPage/Project';
 import MyPortfolio from '@/pages/MyPortfolio/MyPortfolio';
+import Home from '../pages/HomePage/Home.jsx';
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Page />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/upload" element={<Project />} />
       <Route path="/portfolio/:userId" element={<MyPortfolio />} />
-      <Route path='/my-detail/:projectId' element={<MyPortfolio />} />
+      <Route path="/my-detail/:projectId" element={<MyPortfolio />} />
     </Routes>
   );
 };
